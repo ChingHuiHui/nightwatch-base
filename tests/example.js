f30a451ca1a7505ff5c3241b1316142422902762
chinghuihui <sunny70624@gmail.com>
2021-12-03 11:34:01 +0800

    example use assertion

--- tests/example.js
+++ tests/example.js
@@ -1,8 +1,7 @@
 module.exports = {
   Demo(browser) {
     browser
-      .url('https://github.com/ChingHuiHui/nightwatch-base')
-      .expect.element('h1')
-      .text.to.contains('nightwatch-base')
+      .url("https://github.com/ChingHuiHui/nightwatch-base")
+      .assert.containsText("h1", "nightwatch");
   },
-}
+};
